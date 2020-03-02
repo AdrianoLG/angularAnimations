15a4719221f00a93f858a21ff8ff08ca5ad3406b
0016118 <0016118@insags.com>
2020-03-02 13:55:56 +0100

    Animation 4 addtask

--- src/app/animations4/animations/animations.component.ts
+++ src/app/animations4/animations/animations.component.ts
@@ -18,21 +18,37 @@ import { trigger, state, style, animate, transition, keyframes, AnimationEvent }
     ]),
     trigger('note', [
       state('inactive', style({
-        opacity: '0'
+        opacity: '0',
+        transform: 'translateY(100%)'
       })),
       state('active', style({
-        opacity: '1'
+        opacity: '1',
+        transform: 'translateY(0)'
       })),
-      transition('inactive => active', animate('.5s', keyframes([
-        style({ opacity: 0, transform: 'translateY(0)', offset: 0 }),
-        style({ opacity: 1, transform: 'translateY(-15px)', offset: 0.6 }),
-        style({ opacity: 1, transform: 'translateY(0)', offset: 1 }),
-      ]))),
-      transition('active => inactive', animate('.5s', keyframes([
-        style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
-        style({ opacity: 1, transform: 'translateY(-15px)', offset: 0.6 }),
-        style({ opacity: 0, transform: 'translateY(100%)', offset: 1 }),
-      ])))
+      transition('inactive => active', [
+        animate('2s', keyframes([
+          style({ opacity: 0, transform: 'translateY(0)', offset: 0 }),
+          style({ opacity: 1, transform: 'translateY(-15px)', offset: 0.6 }),
+          style({ opacity: 1, transform: 'translateY(0)', offset: 1 })
+        ]))
+      ]),
+      transition('active => inactive', [
+        animate('2s', keyframes([
+          style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
+          style({ opacity: 1, transform: 'translateY(-15px)', offset: 0.6 }),
+          style({ opacity: 0, transform: 'translateY(100%)', offset: 1 })
+        ]))
+      ])
+    ]),
+    trigger('addTask', [
+      state('in', style({ transform: 'translateY(0)' })),
+      transition('void => *', [
+        style({ transform: 'translateY(-100%)', opacity: 0 }),
+        animate('.3s ease-out')
+      ]),
+      transition('* => void', [
+        animate('.3s ease-out', style({ transform: 'scaleY(0) translateY(200px)' }))
+      ])
     ])
   ]
 })
@@ -58,6 +74,31 @@ export class AnimationsComponent4 implements OnInit {
       this.tasks.push(event.target.value);
       event.target.value = '';
     }
+  }
+
+  removeTask(index) {
+    this.tasks.splice(index, 1);
+  }
+
+  onAnimationEvent(event: AnimationEvent) {
+    console.group(event.triggerName);
+
+    // phaseName is start or done
+    console.warn(`Phase: ${event.phaseName}`);
+
+    // in our example, totalTime is 1000 or 1 second
+    console.warn(`Total time: ${event.totalTime}`);
+
+    // in our example, fromState is either open or closed
+    console.warn(`From: ${event.fromState}`);
+
+    // in our example, toState either open or closed
+    console.warn(`To: ${event.toState}`);
+
+    // the HTML element itself, the button in this case
+    console.warn(`Element: ${event.element}`);
+
+    console.groupEnd();
   }
 
 }
